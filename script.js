577cd0cf1cc445c48d3f4a98311dc25eef060c53
Sebastian Majda <sebastianmajda90@gmail.com>
2023-03-14 16:23:15 +0100

    section 3 start

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
-// const name = "Sebastian";
-// const favMeal = "schabowy";
+const text = "123";
+console.log(typeof text);
+const quate = ' He said "ale jazda"';
+console.log(typeof text);
 
-// console.log("Czesc, jestem " + name + " i moje ulubione danie to " + favMeal);
-// console.log(`Cześć, jestem ${name} i moje ulubione danie to ${favMeal}`);
+const msg = "to jest to";
+console.log(msg.toUpperCase());
 
-// const mojeImie = "Sebulba";
-// const imiePsiunka = "Luna";
-// console.log(`Cześć, jestem ${mojeImie} a to jest ${imiePsiunka} - mój pies`);
-
-// let wiekZwierzaka = 1;
-
-// console.log(imiePsiunka + " ma już " + wiekZwierzaka + " rok !");
-
-const firstName = "Tomek";
-let age = 12;
-age = 18;
-
-const favColor = "niebieski";
-const fav_meal = "schabowy";
-
-let currentCar;
-currentCar = "Audi";
-
-console.log(currentCar);
-const secondName = "Ania";
-let age2 = 24;
-let favColor2 = "czerwony";
+const username = "sebastian";
+const newUsername = username.charAt(0).toUpperCase() + username.slice(1);
+console.log(newUsername);
