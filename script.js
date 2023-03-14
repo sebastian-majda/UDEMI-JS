276ca2a63830ee963dbd335629747e30f24d66ea
Sebastian Majda <sebastianmajda90@gmail.com>
2023-03-14 13:59:07 +0100

    Section_2_finished

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
-const name = "Sebastian";
-console.log(name);
+// const name = "Sebastian";
+// const favMeal = "schabowy";
+
+// console.log("Czesc, jestem " + name + " i moje ulubione danie to " + favMeal);
+// console.log(`Cześć, jestem ${name} i moje ulubione danie to ${favMeal}`);
+
+// const mojeImie = "Sebulba";
+// const imiePsiunka = "Luna";
+// console.log(`Cześć, jestem ${mojeImie} a to jest ${imiePsiunka} - mój pies`);
+
+// let wiekZwierzaka = 1;
+
+// console.log(imiePsiunka + " ma już " + wiekZwierzaka + " rok !");
+
+const firstName = "Tomek";
+let age = 12;
+age = 18;
+
+const favColor = "niebieski";
+const fav_meal = "schabowy";
+
+let currentCar;
+currentCar = "Audi";
+
+console.log(currentCar);
+const secondName = "Ania";
+let age2 = 24;
+let ulubionyKolor = "czerwony";
