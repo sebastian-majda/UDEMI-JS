3a7a35c4f84649c248f60fd8f6055bfd7173d7a4
Sebastian Majda <sebastianmajda90@gmail.com>
2023-03-15 16:13:21 +0100

    chapter 4

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const text = "123";
-console.log(typeof text);
-const quate = ' He said "ale jazda"';
-console.log(typeof text);
+// const text = "123";
+// console.log(typeof text);
+// const quate = ' He said "ale jazda"';
+// console.log(typeof text);
 
-const msg = "to jest to";
-console.log(msg.toUpperCase());
+// const msg = "to jest to";
+// console.log(msg.toUpperCase());
 
-const username = "sebastian";
-const newUsername = username.charAt(0).toUpperCase() + username.slice(1);
-console.log(newUsername);
+// const username = "sebastian";
+// const newUsername = username.charAt(0).toUpperCase() + username.slice(1);
+// console.log(newUsername);
+
+const text1 = "powiększ mnie!";
+console.log(text1.toUpperCase());
+const text2 = "ZAPISZ MNIE MAŁYMI LITERAMI";
+console.log(text2.toLowerCase());
+const text3 = "$#%#^ wytnij te dziwne znaki na początku!";
+console.log(text3.slice(6));
+const text4 = 'sprawdź, czy zawieram słowo "czy"';
+console.log(text4.includes("czy"));
+const text5 =
+  'wyLoguj w konsoli tylko literę "L", która znajduje się w wyrazie "Wyloguj"';
+console.log(text5.charAt(2));
+const text6 = "pies zamień każde słowo pies, na słowo kot pies";
+console.log(text6.replaceAll("pies", "kot"));
+const text7 = "podziel, ten, string, od, przecinków";
+console.log(text7.split(","));
