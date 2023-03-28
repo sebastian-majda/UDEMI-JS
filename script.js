ceae3c161c8fcaa347f77fddfa52ecaae8e0d65b
Sebastian Majda <sebastianmajda90@gmail.com>
2023-03-28 16:36:52 +0200

    section 8 ex.2 finished

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-const add = (x, y) => {
-  const score = x + y;
+// const add = (x, y) => {
+//   const score = x + y;
 
-  //   if (score % 2 === 0) {
-  //     function1();
-  //   } else {
-  //     function2();
-  //   }
-  score % 2 === 0 ? function1(score) : function2(score);
+//   //   if (score % 2 === 0) {
+//   //     function1();
+//   //   } else {
+//   //     function2();
+//   //   }
+//   score % 2 === 0 ? function1(score) : function2(score);
+// };
+
+// const function1 = (num) => {
+//   console.log(`Liczba ${num} jest parzysta`);
+// };
+
+// const function2 = (num) => {
+//   console.log(`Liczba ${num} jest nieparzysta`);
+// };
+
+// add(3, 8);
+
+let celcius;
+let temp;
+
+const fahrenheit = (c) => {
+  let celcius = c;
+  let temp = celcius * 1.8 + 32;
+  console.log(`${c} C = ${temp} F`);
 };
-
-const function1 = (num) => {
-  console.log(`Liczba ${num} jest parzysta`);
-};
-
-const function2 = (num) => {
-  console.log(`Liczba ${num} jest nieparzysta`);
-};
-
-add(3, 8);
+fahrenheit(20);
