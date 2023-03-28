226c8672cf9d5f01547b9bb370848baf4453a93e
Sebastian Majda <sebastianmajda90@gmail.com>
2023-03-28 16:52:48 +0200

    ex 3 section 8 FINISHED!! forEach! For! callback

--- script.js
+++ script.js
@@ -19,12 +19,31 @@
 
 // add(3, 8);
 
-let celcius;
-let temp;
+// ex2
+// let celcius;
+// let temp;
 
-const fahrenheit = (c) => {
-  let celcius = c;
-  let temp = celcius * 1.8 + 32;
-  console.log(`${c} C = ${temp} F`);
+// const fahrenheit = (c) => {
+//   let celcius = c;
+//   let temp = celcius * 1.8 + 32;
+//   console.log(`${c} C = ${temp} F`);
+// };
+// fahrenheit(20);
+
+// ex3 FAJNE!
+const num = 10;
+const numbers = [];
+
+for (let i = 0; i < num; i++) {
+  numbers.push(i);
+}
+
+const checker = (number) => {
+  if (number % 3 === 0 && number !== 0) {
+    console.log(`${number} jest podzielne przez 3`);
+  } else {
+    console.log(`${number} nie jest podzielne przez 3 lub ${number}=0`);
+  }
 };
-fahrenheit(20);
+
+numbers.forEach(checker);
