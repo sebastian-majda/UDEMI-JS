0d262ca353a5841cb9e91ff3f72e5db149abe964
Sebastian Majda <sebastianmajda90@gmail.com>
2023-03-28 14:01:21 +0200

    chapter 7 ex 5 finished

--- script.js
+++ script.js
@@ -68,15 +68,28 @@
 // const combinedArray2 = [...numbers, ...animals];
 // console.log(combinedArray2);
 
-const numbers = [1, 5, 13, 26, 48];
+// const numbers = [1, 5, 13, 26, 48];
 
-const newNumbers = numbers.map((x) => x * 5);
-console.log(newNumbers);
+// const newNumbers = numbers.map((x) => x * 5);
+// console.log(newNumbers);
 
-for (const number of newNumbers) {
-  if (number % 2 === 0) {
-    console.log(`liczba parzyste ${number} `);
-  } else {
-    console.log(`liczba nieparzyste ${number} `);
-  }
+// for (const number of newNumbers) {
+//   if (number % 2 === 0) {
+//     console.log(`liczba parzyste ${number} `);
+//   } else {
+//     console.log(`liczba nieparzyste ${number} `);
+//   }
+// }
+
+const colours = ["yellow"];
+colours.push("green");
+colours.unshift("brown");
+console.log(colours);
+
+for (let i = 0; i < colours.length; i++) {
+  console.log(
+    `moj ulubiony kolor to ${
+      colours[i].charAt(0).toUpperCase() + colours[i].slice(1)
+    }`
+  );
 }
