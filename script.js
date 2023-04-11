bd6cc3f82d6988207ba147d9147f3860e3311bd1
Sebastian Majda <sebastianmajda90@gmail.com>
2023-04-11 11:50:49 +0200

    chapter 8

--- script.js
+++ script.js
@@ -1,49 +1,5 @@
-// const add = (x, y) => {
-//   const score = x + y;
-
-//   //   if (score % 2 === 0) {
-//   //     function1();
-//   //   } else {
-//   //     function2();
-//   //   }
-//   score % 2 === 0 ? function1(score) : function2(score);
-// };
-
-// const function1 = (num) => {
-//   console.log(`Liczba ${num} jest parzysta`);
-// };
-
-// const function2 = (num) => {
-//   console.log(`Liczba ${num} jest nieparzysta`);
-// };
-
-// add(3, 8);
-
-// ex2
-// let celcius;
-// let temp;
-
-// const fahrenheit = (c) => {
-//   let celcius = c;
-//   let temp = celcius * 1.8 + 32;
-//   console.log(`${c} C = ${temp} F`);
-// };
-// fahrenheit(20);
-
-// ex3 FAJNE!
-const num = 10;
-const numbers = [];
-
-for (let i = 0; i < num; i++) {
-  numbers.push(i);
-}
-
-const checker = (number) => {
-  if (number % 3 === 0 && number !== 0) {
-    console.log(`${number} jest podzielne przez 3`);
-  } else {
-    console.log(`${number} nie jest podzielne przez 3 lub ${number}=0`);
-  }
-};
-
-numbers.forEach(checker);
+const divik = document.querySelector("div");
+const paragraph = document.createElement("p");
+divik.appendChild(paragraph);
+zmiana;
+zmiana2;
