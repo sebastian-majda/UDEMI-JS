84d8c9b196ebc0273cb914eef3c33fd5e5f10bea
Sebastian Majda <sebastianmajda90@gmail.com>
2023-05-22 14:52:27 +0200

    Powiekszanie czcionki i zmiana kolor paragrafu

--- Powiększ, pomniejsz i zmień kolor/script.js
+++ Powiększ, pomniejsz i zmień kolor/script.js
@@ -2,5 +2,29 @@ const sizeUp = document.querySelector(".sizeUp");
 const sizeDown = document.querySelector(".sizeDown");
 const color = document.querySelector(".color");
 const p = document.querySelector("p");
+let fontSize = 36;
 
-const size = (params) => {};
+const increase = (params) => {
+  fontSize += 5;
+  p.style.fontSize = fontSize + "px";
+};
+
+const decrease = (params) => {
+  if (fontSize <= 21) {
+    return;
+  }
+  fontSize -= 5;
+  p.style.fontSize = fontSize + "px";
+};
+
+const colorChange = () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  p.style.color = `rgb(${r},${g},${b})`;
+};
+
+color.addEventListener("click", colorChange);
+sizeUp.addEventListener("click", increase);
+sizeDown.addEventListener("click", decrease);
