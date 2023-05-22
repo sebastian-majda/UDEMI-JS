b63d0c826983afd309edbbb1f962a52b4eb0b8df
Sebastian Majda <sebastianmajda90@gmail.com>
2023-05-22 13:38:16 +0200

    104 project 2

---
+++ Powiększ, pomniejsz i zmień kolor/script.js
@@ -0,0 +1,6 @@
+const sizeUp = document.querySelector(".sizeUp");
+const sizeDown = document.querySelector(".sizeDown");
+const color = document.querySelector(".color");
+const p = document.querySelector("p");
+
+const size = (params) => {};
