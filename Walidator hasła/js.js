221832ce0b361e7be7863807d4f59c9e005f9659
Sebastian Majda <sebastianmajda90@gmail.com>
2023-05-22 15:48:48 +0200

    Walidator Hasła ! ważne

--- Walidator hasła/js.js
+++ Walidator hasła/js.js
@@ -1,6 +1,30 @@
-const pass = document.querySelector('#password');
-const p = document.querySelector('.passinfo');
+const pass = document.querySelector("#password");
+const p = document.querySelector(".passinfo");
 const letters = /[a-z]/i;
 const numbers = /[0-9]/;
 const special = /[!@#$%^&*()]/;
 const minValue = 10;
+
+const showMsg = () => {
+  if (
+    pass.value.length > minValue &&
+    pass.value.match(letters) &&
+    pass.value.match(numbers) &&
+    pass.value.match(special)
+  ) {
+    p.textContent = "Masz bardzo dobre hasło!";
+    p.style.color = "lime";
+  } else if (
+    pass.value.length > minValue &&
+    pass.value.match(letters) &&
+    pass.value.match(numbers)
+  ) {
+    p.textContent = "Masz  dobre hasło!";
+    p.style.color = "gold";
+  } else {
+    p.textContent = "Masz  niepoprawne hasło!";
+    p.style.color = "hotpink";
+  }
+};
+
+pass.addEventListener("keyup", showMsg);
