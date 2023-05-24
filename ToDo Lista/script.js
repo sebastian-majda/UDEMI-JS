1eb90b2842349049117fe4f37e256ce172060d4f
Sebastian Majda <sebastianmajda90@gmail.com>
2023-05-24 21:34:34 +0200

    added quaryselectors

--- ToDo Lista/script.js
+++ ToDo Lista/script.js
@@ -1,4 +1,25 @@
-let todoInput //miejsc gdzie uzytkownik wpisuje tresc zadania
-let errorInfo // info o braku zadan koniecznosc wprowadzenia tekstu
-let 
-let
+let todoInput; //miejsc gdzie uzytkownik wpisuje tresc zadania
+let errorInfo; // info o braku zadan koniecznosc wprowadzenia tekstu
+let addBtn; //przycisk add
+let ulList; //lista zadan, tagi ul
+
+const main = () => {
+  //wywoływanie funkcji
+  prepareDomElements();
+  prepareDomEvents();
+};
+
+const prepareDomElements = () => {
+  //pobieranie wszystkich elementów
+  todoInput = document.querySelector(".todo-input");
+  errorInfo = document.querySelector(".error-info");
+  addBtn = document.querySelector(".btn-add");
+  ulList = document.querySelector(".todolist ul");
+};
+
+const prepareDomEvents = () => {
+  //nadawanie zasłuchiwania
+};
+
+document.addEventListener("DOMContentLoaded", main);
+//po załadowaniu całej strony, otwór funkcje main
